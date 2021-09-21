9437a3a215a98a8e579b15b6b6a6512999b0bf53
Brian Egolf <47579932+ObeyedSky622@users.noreply.github.com>
2021-09-21 17:56:19 -0400

    Add files via upload

---
+++ template.py
@@ -0,0 +1,11 @@
+import os 
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import plaidml as pml 
+import numpy as np 
+import pandas
+import matplotlib.pyplot as plt 
+import keras
+def main():
+
+    print("hello world")
+main()
