dce167c6f30d54f4409a446c386a53a222612da9
Brian Egolf <47579932+ObeyedSky622@users.noreply.github.com>
2021-09-21 23:01:59 -0400

    Add files via upload
    Script to output model to JSON file

---
+++ src/first.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import keras 
+from keras.layers import Dense
+from keras.models import Sequential
+from keras import Model
+model_name = "my_first_model"
+
+fileOut = open(model_name+".json",'w')
+
+myModel = Sequential()
+myModel.add(Dense(16, input_dim=16,activation='relu'))
+myModel.add(Dense(12, activation='relu'))
+myModel.add(Dense(4,  activation='softmax'))
+
+old = ","
+new = ",\n"
+fileOut.write(Model.to_json(myModel).replace(old,new))
+#print(len(myModel.layers))
+for layer in myModel.layers:
+    print("Layer Name: "+layer.name)
+    myModel.get_layer(layer.name)
+    print("Layer input shape: ")
+    print(layer.input_shape)
+    print("Layer Output shape: ")
+    print(layer.output_shape)
+    print("-------------------")
+#print(type(myModel.summary()))
