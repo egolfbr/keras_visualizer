2732f588e321a2ead796b5912cf787e86909a74e
Brian Egolf <47579932+ObeyedSky622@users.noreply.github.com>
2021-12-02 16:08:13 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ setup(name="keras_dot_visualizer",
       description="Front-end Keras Visualizer",
       author="Brian Egolf",
       author_email="egolfbr@miamioh.edu,
-      url="",
+      url="https://github.com/ObeyedSky622/keras_visualizer",
       package_dir = {'':'src'}
       py_modules = ["writedot"]
   
