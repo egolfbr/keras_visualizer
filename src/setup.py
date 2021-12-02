7b61ad2c4f75adebd3392a8ed75f436cb37dfe1c
Brian Egolf <47579932+ObeyedSky622@users.noreply.github.com>
2021-12-02 16:07:54 -0500

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup 
+
+setup(name="keras_dot_visualizer",
+      version="0.0.1",
+      description="Front-end Keras Visualizer",
+      author="Brian Egolf",
+      author_email="egolfbr@miamioh.edu,
+      url="",
+      package_dir = {'':'src'}
+      py_modules = ["writedot"]
+  
+)
