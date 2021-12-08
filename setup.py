53731c4477bdbc83db2ad1ca3c7514e4b2321079
Brian Egolf <47579932+egolfbr@users.noreply.github.com>
2021-12-08 13:41:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup 
 
 setup(name="keras_dot_visualizer",
-      version="0.0.4",
+      version="0.0.5",
       description="Front-end Keras Visualizer",
       author="Brian Egolf",
       author_email="egolfbr@miamioh.edu",
