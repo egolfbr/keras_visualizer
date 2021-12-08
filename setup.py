28f1c349439b05ca071947b1312a4a3acee714b9
Brian <egolfbr@miamioh.edu>
2021-12-07 19:05:30 -0500

    updated folder names

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup 
 
 setup(name="keras_dot_visualizer",
-      version="0.0.3",
+      version="0.0.4",
       description="Front-end Keras Visualizer",
       author="Brian Egolf",
       author_email="egolfbr@miamioh.edu",
       url="https://github.com/ObeyedSky622/keras_visualizer",
-      package_dir = {'':'src'},
+      package_dir = {'':'keras-dot-visualizer'},
       py_modules = ["writedot"]
   
 )
